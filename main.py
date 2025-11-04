5f5a2b5742054f09d0ab040acf5589a1c93f2fa3
EEdoolotu <ejirogheneedoolotu@gmail.com>
2025-11-04 06:14:28 +0000

    new

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 from constants import * 
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     clock = pygame.time.Clock()
     
@@ -15,7 +17,9 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
 
         dt = clock.tick(60) / 1000
 
